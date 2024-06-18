f011f05d3003fab30899e3ba2e6f9f9e1c2cb40a
Matheus Martines <matheus27martines@gmail.com>
2024-06-18 00:51:35 -0300

    DecisionTree for Quark-Gluon Tagging

--- src/Preprocessing/LabelsGeneration.py
+++ src/Preprocessing/LabelsGeneration.py
@@ -1,5 +1,4 @@
-from typing import List, Tuple, Dict
-from src.Particle import ParticleType
+from typing import List, Tuple
 import numpy as np
 
 
@@ -39,18 +38,17 @@ def create_jet_labels_one_column_per_category(jet_type_indices: List[Tuple[int, 
     return jet_labels
 
 
-def create_labels_single_column(jet_inputs: Dict[ParticleType, Tuple[int, int]]):
+def create_labels_single_column(jet_type_indices: List[Tuple[int, int]]):
     """
     Creates the Jet labels using a single column and diferent integer values for different categories.
 
-    The jet_inputs is a dictionary where the key stores the type of jet (Gluon, Top, or LightQuarks), while
-    the value stores a tuple with the ranges of each jet in the data X. For example, if X has first Top jets,
-    and then Gluon jets, the dictionary would look like:
+    The jet_inputs is a list where the key stores a tuple with the ranges of each jet in the data X.
+    For example, if X has first Top jets, and then Gluon jets, the list would look like:
 
-        {
-            ParticleType.Top: (index of the first top jet, index of the last top jet),
-            ParticleType.Gluon: (index of the first gluon jet, index of the last gluon jet)
-        }
+        [
+            (index of the first top jet, index of the last top jet),
+            (index of the first gluon jet, index of the last gluon jet)
+        ]
 
     This class assumes that the data X is ordered by the type of the jet. So, X must look like the following
 
@@ -58,21 +56,22 @@ def create_labels_single_column(jet_inputs: Dict[ParticleType, Tuple[int, int]])
             Top jet, Top jet, Top jet, ..., Last Top jet, Gluon jet, ..., Last Gluon jet
         ]
 
-    The first jet in the dictionary is assigned to the category 0, the second to category 1, and so on.
+    The first jet in the dictionary is assigned to the category the number of jet categories - 1,
+    the second to category 1, and so on.
 
-    :param jet_inputs: dictionary with the information needed to create the jet labels.
+    :param jet_type_indices: list with the information needed to create the jet labels.
     :return: the labels in the same order as X.
     """
     # counting the total number of jets
-    number_of_jets = sum([final_index - initial_index + 1 for initial_index, final_index in jet_inputs.values()])
-    # creating the array of the same lenght as the data and with the number of columns
-    # equal to the number of jet types
+    number_of_jets = sum([final_index - initial_index + 1 for initial_index, final_index in jet_type_indices])
+    # array that will store the label of each jet
     jet_labels = np.zeros(number_of_jets)
-    limit_categories = list(jet_inputs.values())
-    # filling the jet labels (starting from the category 1 since the category 0 is already done)
-    for jet_index, indices in zip(range(1, len(jet_inputs)), limit_categories[1:]):
-        jet_labels[indices[0]: indices[1] + 1] = jet_index
+    # the first label is the number of categories - 1
+    category_label = len(jet_type_indices) - 1
+    # filling the categories
+    # note that we do not need to go up to the last category since the jet labels entries are already set
+    # to zero
+    for initial_index, final_index in jet_type_indices[:-1]:
+        jet_labels[initial_index: final_index + 1] = category_label
+        category_label -= 1
     return jet_labels
-
-
-
